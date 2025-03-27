8b983a8804d65261e4377c93430d2a5ffa3ed3a8
momenala7sere <39879739+momenala7sere@users.noreply.github.com>
2025-03-27 16:15:16 +0300

    Initial commit

---
+++ lib/common/basewidgets/custom_button_widget.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:sixvalley_vendor_app/utill/dimensions.dart';
+import 'package:sixvalley_vendor_app/utill/styles.dart';
+
+class CustomButtonWidget extends StatelessWidget {
+  final Function? onTap;
+  final String? btnTxt;
+  final bool isColor;
+  final Color? backgroundColor;
+  final Color? fontColor;
+  final Color? borderColor;
+  final double? borderRadius;
+  const CustomButtonWidget({Key? key, this.onTap, required this.btnTxt, this.backgroundColor, this.isColor = false, this.fontColor, this.borderRadius, this.borderColor}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap as void Function()?,
+      child: Container( height: 40, alignment: Alignment.center,
+        decoration: BoxDecoration(
+          color: isColor? backgroundColor : backgroundColor ?? Theme.of(context).primaryColor,
+          borderRadius: BorderRadius.circular(borderRadius != null? borderRadius! : Dimensions.paddingSizeExtraSmall),
+          border: Border.all(color: borderColor ?? Theme.of(context).cardColor)
+        ),
+        child: Text(btnTxt!,
+            style: robotoMedium.copyWith(
+              fontSize: Dimensions.fontSizeDefault,
+              color: fontColor ?? Colors.white,
+            )),
+      ),
+    );
+  }
+}
